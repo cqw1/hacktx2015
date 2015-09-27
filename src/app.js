f47a38c293c6edeb4e92bbb1950b0646e4414e70
cqw1 <cassie.q.wang@gmail.com>
2015-09-26 21:58:20 -0500

    added stuff

--- src/app.js
+++ src/app.js
@@ -1,84 +1,118 @@
-/**
- * Welcome to Pebble.js!
- *
- * This is where you write your app.
- */
-
 var UI = require('ui');
 var ajax = require('ajax');
-
-var cityName = 'Austin';
-var URL = 'http://api.openweathermap.org/data/2.5/weather?q=' + cityName;
-
-ajax(
-  {
-    url: URL,
-    type: 'json'
-  },
-  function(data) {
-    // Success!
-    console.log('Successfully fetched weather data!');
-    
-    // Extract data
-    var location = data.name;
-    var temperature = Math.round(data.main.temp - 273.15) + 'C';
-    
-    // Always upper-case first letter of description
-    var description = data.weather[0].description;
-    description = description.charAt(0).toUpperCase() + description.substring(1);
-    
-    card.subtitle(location + ',' + temperature);
-    card.body(description);
-  },
-  function(error) {
-    // Error!
-    console.log('Failed fetching weather data: ' + error);
-  }
-);
+var Accel = require('ui/accel');
 
 // CONFIGURATION
 
-// var accelData = [];
+var accelData = [];
 
-// var Accel = require('ui/accel');
-// Accel.init();
-// // rate, samples, subscribe
-// Accel.config(100, 25, true);
-// Accel.on('data', function(e) {
-//     accelData.push(JSON.stringify(e.accels));
-//     console.log(accelData);
-// });
-
-// Create the trainer/trainee menu
-var menu = new UI.Menu({
-  sections: []
+Accel.init();
+// rate, samples, subscribe
+Accel.config(100, 25, true);
+Accel.on('data', function(e) {
+//     if (window_is_loaded(trainer_window)) {
+      accelData.push(JSON.stringify(e.accels));
+      console.log(accelData);
+//     }
+    
 });
 
-var trainer_section = {
-  title: 'Options',
-  items: [{
-    title: 'Trainer'
-  }]
-};
-var trainee_section = {
-  items: [{
-    title: 'Trainee'
-  }]
-};
 
-menu.section(1, trainer_section);
-menu.section(2, trainee_section);
+var roleOptions = [
+  {
+    title: 'Trainer'
+  },
+  {
+    title: 'Trainee'
+  }
+];
+
+var gestures = [
+  {
+    title: 'Line'
+  }
+  
+];
+
+var menu = new UI.Menu({
+  sections:[{
+    title: 'Options',
+    items: roleOptions
+  }]
+});
 menu.show();
 
-var trainer_window = new UI.Card({
-    title: 'Trainer Mode'
-    });
-  trainer_window.body('Start moving!');
-
-  var trainee_window = new UI.Card({
-    title: 'Trainee Mode!'
+menu.on('select', function (e) {
+  var roleDetails= new UI.Menu({
+    sections:[{
+      title: roleOptions[e.itemIndex].title,
+      items: gestures
+    }]
+  })
+  
+  var addSection = {
+    items: [{
+      title: 'Add Gesture'
+    }]
+  };
+  
+  var new_gesture = new UI.Card({
+    title: "Add a New Gesture!",
+    subtitle: "Start recording a new action."
   });
-  trainee_window.body('Start learning!');
+  
+  if (roleOptions[e.itemIndex].title == 'Trainer') {
+    roleDetails.section(1, addSection);
+//     roleDetails.item(0, roleDetails.section(0).items.length, {title: '+ Add Gesture'});
+  }
+  
+  roleDetails.show();
+  roleDetails.on('select', function (e) {
+    if (e.item.title == 'Add Gesture') {
+      new_gesture.show();
+    }
+  });
+
+});
+
+
+
+
+
+// // Create the trainer/trainee menu
+// function create_elements() {
+//   var menu = new UI.Menu({
+//     sections: []
+//   });
+
+//   var trainer_section = {
+//     title: 'Options',
+//     items: [{
+//       title: 'Trainer'
+//     }]
+//   };
+  
+//   var trainee_section = {
+//     items: [{
+//       title: 'Trainee'
+//     }]
+//   };
+  
+//   var trainer_window = new UI.Card({
+//     title: 'Trainer Mode'
+//     });
+//   trainer_window.body('Start moving!');
+
+//   var trainee_window = new UI.Card({
+//     title: 'Trainee Mode!'
+//   });
+//   trainee_window.body('Start learning!');
+
+//   // Add sections to the menu.
+//   menu.section(1, trainer_section);
+//   menu.section(2, trainee_section);
+// }
+
 
 // EVENT HANDLERS
 
@@ -87,42 +121,19 @@ var trainer_window = new UI.Card({
 //     array.push(JSON.stringify(e.accels));
 //   }); 
 // }
-
-menu.on('select', function (e) {
   
-  if (e.item.title == 'Trainer') {
-    // start trainer window
-    trainer_window.show();
-    trainee_window.hide();
-    var accelData = [];
-
-var Accel = require('ui/accel');
-Accel.init();
-// rate, samples, subscribe
-Accel.config(100, 25, true);
-Accel.on('data', function(e) {
-    accelData.push(JSON.stringify(e.accels));
-    console.log(accelData);
-});
+//     var accelData = [];
+//     var Accel = require('ui/accel');
+//     Accel.init();
+//     // rate, samples, subscribe
+//     Accel.config(100, 25, true);
+//     Accel.on('data', function(e) {
+//       accelData.push(JSON.stringify(e.accels));
+//       console.log(accelData);
+//     });
+    
 //     start_reading_data(accelData);
 //     var accelData = [];
 //     start_reading_data(accelData, Accel);
 //     console.log("ALL DATA: ", accelData);
-  } else {
-    // start trainee window
-    trainer_window.hide();
-    trainee_window.show();
-  }
-});
-
-
-// Create the Weather Card
-// var card = new UI.Card({
-//   title: 'Weather',
-//   subtitle:'substitle...'
-  
-// });
-
-// // Display the Card
-// card.show();
 
